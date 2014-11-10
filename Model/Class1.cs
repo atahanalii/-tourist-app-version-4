9f453e215470ae8a4b607f7e4c01a95aca96e137
happyprayash <happyprayash@gmail.com>
2014-11-10 13:20:09 +0100

    View Folders are created

---
+++ Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tourist_app_ver_4.Model
+{
+    class Class1
+    {
+    }
+}
